9879f7274b8492c51474aee76c3f1710f5b7eb7b
Eddie Kuang <67530038+ekuang7@users.noreply.github.com>
2025-05-27 19:54:59 -0700

    user.js

--- Credit/routes/user.js
+++ Credit/routes/user.js
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {createUser, login} from '../data/user.js';
+import nodemailer from 'nodemailer';
 
 const router = Router();
 
@@ -115,6 +116,43 @@ router
 router.route('/error')
 .get(async(req,res)=>{
     return res.redirect('/');
+});
+
+router
+.route('/contact')
+.get(async(req,res)=>{
+    return res.render('user/contact');
+})
+
+.post(async(req,res)=>{
+
+    let {name, email, message} = req.body;
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.ADMIN_EMAIL,
+            pass:process.env.ADMIN_PASSWORD,
+        },
+    });
+
+    const sendmessage={
+        from:process.env.ADMIN_EMAIL,
+        to:process.env.SUPPORT_EMAIL,
+        subject: `Support Request`,
+        text: `Name: ${name} \n\nContact Email: ${email} \n\nMessage: ${message}`
+    }
+    try{
+        await transporter.sendMail(sendmessage);
+        res.status(200).send("Message was successfully received.");
+    }
+    catch(error){
+        console.error('Email did not send.', error);
+        res.status(500).send("Sent failed.");
+    }
+    
+
+    
+    
 })
 
 
