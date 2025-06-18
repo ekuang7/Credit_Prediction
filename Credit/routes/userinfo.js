d82e4bb6e785aadffad922175f0a5503e9b156fd
Eddie Kuang <67530038+ekuang7@users.noreply.github.com>
2025-06-18 00:47:36 -0700

    userinfo.js

--- Credit/routes/userinfo.js
+++ Credit/routes/userinfo.js
@@ -1,7 +1,7 @@
 import {Router} from 'express';
 import axios from 'axios';
 import cors from 'cors';
-import {update_status, getAll, average, median} from '../data/user.js';
+import {update_status, getAll, average, median, getApproved, getDenied, notApplied} from '../data/user.js';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
@@ -59,10 +59,17 @@ router.route('/declined')
 //Usercredit route to render usercredit page.
 router.route('/usercredit')
 .get(async(req,res)=>{
+    
     try{
         if(!req.session.user){
              return res.redirect('/');
         }
+
+        if(req.session.user.username==='admin'){
+            return res.redirect('/credit/adminmain');
+        }
+
+       
 
         else{
             if(req.session.user.status===0){
@@ -168,8 +175,27 @@ router.route('/info')
     const approved = {"approved": information.approved};
     const denied = {"denied":information.denied};
     const respond = await axios.post('http://localhost:5002/visualize', {"data":data, "approved":approved, "denied":denied});
+
     
     return res.render('credit/info', {total: total, approved:total_approved, denied:total_denied , approved_avg:mean_approved.mean, denied_avg: mean_denied.mean, approved_med: median_approved.median, denied_med: median_denied.median});
+})
+
+router.route('/adminmain')
+.get(async(req,res)=>{
+    if(!req.session.user){
+        return res.redirect('/');
+    }
+
+    if(req.session.user.username !== 'admin'){
+        return res.render('user/forbidden');
+    }
+
+    const approved =await getApproved();
+    
+    const denied = await getDenied();
+    const none = await notApplied();
+    
+    return res.render('credit/adminmain', {approved:approved.approved, denied:denied.denied, none:none.none});
 })
 
 
